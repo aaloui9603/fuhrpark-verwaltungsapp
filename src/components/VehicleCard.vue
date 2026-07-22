d5819eb16b682255d1527057c2e49553376920f9
aaloui9603 <aaloui.9603@gmail.com>
2026-07-22 17:30:10 +0200

    feat: TÜV-Termin-Feld je Fahrzeug (M16) inkl. UPDATE-Policy vehicles

--- src/components/VehicleCard.vue
+++ src/components/VehicleCard.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { ref } from 'vue'
 import { useAuthStore } from '../stores/authStore'
+import { useVehicleStore } from '../stores/vehicleStore'
 
 const props = defineProps({
   vehicle: {
@@ -11,6 +13,12 @@ const props = defineProps({
 const emit = defineEmits(['edit', 'delete'])
 
 const authStore = useAuthStore()
+const vehicleStore = useVehicleStore()
+const inspectionDate = ref(props.vehicle.inspection_date)
+
+async function saveInspectionDate() {
+  await vehicleStore.updateVehicle(props.vehicle.id, { inspection_date: inspectionDate.value})
+}
 </script>
 
 <template>
@@ -18,7 +26,10 @@ const authStore = useAuthStore()
     <p>{{ vehicle.make_model }} — {{ vehicle.color }}</p>
     <p>{{ vehicle.licence_plate }}</p>
 
-    <div v-if="authStore.role === 'admin'">
+    <div v-if="authStore.role === 'admin' || authStore.role === 'coordinator'">
+      <label>TÜV-Termin</label>
+      <input type="date" v-model="inspectionDate" />
+      <button @click="saveInspectionDate">Speichern</button>
       <button @click="$emit('edit', vehicle)">Bearbeiten</button>
       <button @click="$emit('delete', vehicle.id)">Löschen</button>
     </div>
